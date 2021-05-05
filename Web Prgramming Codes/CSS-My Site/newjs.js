8c28c2fed70179cb3658c33d24010a02a2001fb7
ernestlipson <60032919+ernestlipson@users.noreply.github.com>
2021-05-05 13:00:37 +0000

    Add files via upload
    Udemy Web Development with Angela Yu

---
+++ Web Prgramming Codes/CSS-My Site/newjs.js
@@ -0,0 +1,9 @@
+Document.write("text...: DOMString")
+console.log('many writing styles detected')
+alert("message?: DOMString")
+
+// creating a function
+function myfunction()
+
+var text = new Array(12)
+text = []
